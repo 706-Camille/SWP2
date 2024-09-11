414ac91115f00f4be2094271a48af792b70fe74c
seung min <rlthro2@gmail.com>
2024-09-11 22:05:13 +0900

    first Assignment

---
+++ 04_example_2/04_example_2.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  while(!Serial)
+  {
+    ; // Waiting
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello World!");
+  delay(1000);
+
+}
